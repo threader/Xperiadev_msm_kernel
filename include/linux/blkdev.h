cdcac193f73ebd47ca2a1ebd966cccc97218946c
threader <michael.heltne@gmail.com>
2021-03-12 17:44:03 +0100

    ranchu: fix compiling for ranchu_defconfig
    (cherry picked from commit 73959595ef62e55e6d7ebecb2d810e02e9c1f3f6)

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -362,7 +362,7 @@ struct request_queue {
 	 */
 	struct kobject kobj;
 
-#ifdef CONFIG_PM_RUNTIME
+#ifdef CONFIG_PM
 	struct device		*dev;
 	int			rpm_status;
 	unsigned int		nr_pending;
@@ -979,12 +979,13 @@ extern void blk_put_queue(struct request_queue *);
 /*
  * block layer runtime pm functions
  */
-#ifdef CONFIG_PM_RUNTIME
+#ifdef CONFIG_PM
 extern void blk_pm_runtime_init(struct request_queue *q, struct device *dev);
 extern int blk_pre_runtime_suspend(struct request_queue *q);
 extern void blk_post_runtime_suspend(struct request_queue *q, int err);
 extern void blk_pre_runtime_resume(struct request_queue *q);
 extern void blk_post_runtime_resume(struct request_queue *q, int err);
+extern void blk_set_runtime_active(struct request_queue *q);
 #else
 static inline void blk_pm_runtime_init(struct request_queue *q,
 	struct device *dev) {}
