a9865ff00b98eb44162523012242dde0269d6869
threader <michael.heltne@gmail.com>
2021-06-28 02:35:11 +0200

    Revert "Revert "arm64: Delay ELF HWCAP initialisation until all CPUs are up""
    This reverts commit 03b670f3eac28b95f5ba6ba64dfcf23a37e6743c.

--- arch/arm64/kernel/setup.c
+++ arch/arm64/kernel/setup.c
@@ -329,6 +329,25 @@ void __init setup_cpu_features(void)
 #endif
 }
 
+static void __init setup_processor(void)
+{
+	struct cpu_info *cpu_info;
+
+	cpu_info = lookup_processor_type(read_cpuid_id());
+	if (!cpu_info) {
+		printk("CPU configuration botched (ID %08x), unable to continue.\n",
+		       read_cpuid_id());
+		while (1);
+	}
+
+	cpu_name = cpu_info->cpu_name;
+
+	printk("CPU: %s [%08x] revision %d\n",
+	       cpu_name, read_cpuid_id(), read_cpuid_id() & 15);
+
+	sprintf(init_utsname()->machine, ELF_PLATFORM);
+}
+
 static void __init setup_machine_fdt(phys_addr_t dt_phys)
 {
 	if (!dt_phys || !early_init_dt_scan(phys_to_virt(dt_phys))) {
