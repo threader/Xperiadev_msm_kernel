d2746751fbbb13a99177923175238da3dfbf51dd
threader <michael.heltne@gmail.com>
2021-08-09 04:52:13 +0200

    kernel: missed a merge conflict

--- kernel/cpuset.c
+++ kernel/cpuset.c
@@ -1419,27 +1419,6 @@ out_unlock:
 	return ret;
 }
 
-<<<<<<< HEAD
-static int cpuset_allow_attach(struct cgroup *cgrp,
-			       struct cgroup_taskset *tset)
-{
-	const struct cred *cred = current_cred(), *tcred;
-	struct task_struct *task;
-
-	cgroup_taskset_for_each(task, cgrp, tset) {
-		tcred = __task_cred(task);
-
-		if ((current != task) && !capable(CAP_SYS_ADMIN) &&
-		    cred->euid.val != tcred->uid.val &&
-		    cred->euid.val != tcred->suid.val)
-			return -EACCES;
-	}
-
-	return 0;
-}
-
-=======
->>>>>>> b23287c99258a (CHROMIUM: remove Android's cgroup generic permissions checks)
 static void cpuset_cancel_attach(struct cgroup *cgrp,
 				 struct cgroup_taskset *tset)
 {
