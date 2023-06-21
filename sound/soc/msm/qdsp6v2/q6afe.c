236ced321a42811c7f669037b75d5da42047379b
threader <michael.heltne@gmail.com>
2023-06-21 11:02:04 +0200

    fix merge bug and follow a commit for q6afe

--- sound/soc/msm/qdsp6v2/q6afe.c
+++ sound/soc/msm/qdsp6v2/q6afe.c
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-/* Copyright (c) 2012-2019, The Linux Foundation. All rights reserved.
-=======
 /* Copyright (c) 2012-2016, 2019 The Linux Foundation. All rights reserved.
->>>>>>> caf_migration/LA.BR.1.3.7.c26
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
